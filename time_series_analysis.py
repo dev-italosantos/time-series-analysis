166d9e3253dcbcb9acdd1cd0e4fdcd6ee5549af4
dev-italosantos <italotayllor@gmail.com>
2023-06-10 23:56:49 -0300

    feat: Ibovespa time interval in 5 years with the current date as the limit

--- time_series_analysis.py
+++ time_series_analysis.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import matplotlib as plt
 import yfinance as yt
+import datetime
+
+data_atual = datetime.datetime.now().date()
 
 ticket = yt.Ticker('^BVSP')
-df = ticket.history(period = '5y', interval = '1d')
 
-print(df)
+df = ticket.history(interval='1d', start='2018-01-01', end=data_atual)
+
+print(df.tail(2))
+
