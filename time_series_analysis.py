302fd0a0ff5c226711d226f13074e6cd1683cde1
dev-italosantos <italotayllor@gmail.com>
2023-06-11 19:50:59 -0300

    refactor: modify active

--- time_series_analysis.py
+++ time_series_analysis.py
@@ -3,12 +3,12 @@ import matplotlib as plt
 import yfinance as yt
 import datetime
 
-data_atual = datetime.datetime.now().date()
+end_date = datetime.datetime.now().date()
 start_date = '2018-01-01'
 
 ticket = yt.Ticker('^BVSP')
 
-df = ticket.history(interval='1d', start=start_date, end=data_atual)
+df = ticket.history(interval='1d', start=start_date, end=end_date)
 
 print(df[['Close']])
 print(df.tail(2))
