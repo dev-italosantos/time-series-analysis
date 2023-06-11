ff33bdf81a4773e6d196f6fd2bd059c2588a2b6d
dev-italosantos <italotayllor@gmail.com>
2023-06-11 17:20:26 -0300

    refactor: modification of the start date for searching for assets

--- time_series_analysis.py
+++ time_series_analysis.py
@@ -4,11 +4,11 @@ import yfinance as yt
 import datetime
 
 data_atual = datetime.datetime.now().date()
+start_date = '2018-01-01'
 
 ticket = yt.Ticker('^BVSP')
 
-df = ticket.history(interval='1d', start='2018-01-01', end=data_atual)
+df = ticket.history(interval='1d', start=start_date, end=data_atual)
 
 print(df[['Close']])
 print(df.tail(2))
-
