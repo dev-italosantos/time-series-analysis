2a3309ec5bfd2e5b364da21475959bf097cf59fa
dev-italosantos <italotayllor@gmail.com>
2023-06-11 18:27:58 -0300

    feat: Obtaining the dates and amounts of dividends for each asset reported for the last 5 years

--- best_dividend_paying_stocks.py
+++ best_dividend_paying_stocks.py
@@ -1,49 +1,22 @@
-# import yfinance as yf
-
-# # Define a lista de ações do Ibovespa
-# tickers = ['VIVT3 .SA', 'CSAN3.SA', 'SANB11.SA', 'SANB11.SA']  # Exemplo com algumas ações
-
-# # Itera sobre cada ticker e obtém os dados de dividendos
-# for ticker in tickers:
-#     try:
-#         # Obtém os dados de dividendos usando o yfinance
-#         data = yf.Ticker(ticker)
-#         dividendos = data.dividends
-
-#         # Filtra os dividendos por mês e obtém as datas de pagamento
-#         dividendos_por_mes = dividendos.groupby(dividendos.index.to_period('M')).apply(lambda x: list(x.index))
-
-#         # Imprime as informações das ações que pagaram dividendos por mês
-#         print(f"Dividendos para o ticker {ticker}:")
-#         for mes, datas in dividendos_por_mes.items():
-#             print(f"Mês: {mes}")
-#             for data in datas:
-#                 print(f"Data de pagamento: {data.date()}, Ativo: {ticker}")
-#     except:
-#         pass
-
-
 import yfinance as yf
+from datetime import datetime, timedelta
 
-# Define a lista de ações do Ibovespa
-tickers = ['PETR4.SA', 'VALE3.SA', 'ITUB4.SA', 'BBDC4.SA', 'ABEV3.SA']  # Exemplo com algumas ações
+tickers = ['VIVT3.SA', 'CSAN3.SA', 'SANB11.SA', 'SANB11.SA']  # Exemplo com algumas ações
 
-# Itera sobre cada ticker e obtém os dados de dividendos
+data_atual = datetime.now().date()
+
+data_limite = (data_atual - timedelta(days=5*365))
+
 for ticker in tickers:
     try:
-        # Obtém os dados de dividendos usando o yfinance
         data = yf.Ticker(ticker)
         dividendos = data.dividends
 
-        # Filtra os dividendos por mês e obtém as datas de pagamento e valores
-        dividendos_por_mes = dividendos.groupby(dividendos.index.to_period('M')).apply(lambda x: list(zip(x.index, x)))
+        dividendos_ultimos_cinco_anos = [dividendo for dividendo in dividendos.items() if data_limite <= dividendo[0].date() <= data_atual]
 
-        # Imprime as informações das ações que pagaram dividendos por mês
         print(f"Dividendos para o ticker {ticker}:")
-        for mes, dados in dividendos_por_mes.items():
-            print(f"Mês: {mes}")
-            for data, valor in dados:
-                print(f"Data de pagamento: {data.date()}, Ativo: {ticker}, Valor: {valor:.2f}")
+        for data, valor in dividendos_ultimos_cinco_anos:
+            print(f"Data de pagamento: {data.date()}, Ativo: {ticker}, Valor: {valor:.2f}")
     except:
         pass
 
